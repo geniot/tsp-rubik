c4ae836fea7644f2387b2e62a654c2ac72fe796d
Vitali Sazanovich <vitali.sazanovich@koop.overheid.nl>
2025-05-08 09:31:14 +0200

    Make two black colors for contract

--- src/main.go
+++ src/main.go
@@ -44,15 +44,16 @@ const (
 
 // https://www.schemecolor.com/rubik-cube-colors.php
 var (
-	black     = rl.Color{R: 0, G: 0, B: 0, A: 255}
-	green     = rl.Color{R: 0, G: 155, B: 72, A: 255}
-	red       = rl.Color{R: 185, G: 0, B: 0, A: 255}
-	blue      = rl.Color{R: 0, G: 69, B: 173, A: 255}
-	orange    = rl.Color{R: 255, G: 89, B: 0, A: 255}
-	white     = rl.Color{R: 255, G: 255, B: 255, A: 255}
-	yellow    = rl.Color{R: 255, G: 213, B: 0, A: 255}
-	allColors = map[int]rl.Color{
-		blackKey:  black,
+	black      = rl.Color{R: 0, G: 0, B: 0, A: 255}
+	lightBlack = rl.Color{R: 77, G: 77, B: 77, A: 255}
+	green      = rl.Color{R: 0, G: 155, B: 72, A: 255}
+	red        = rl.Color{R: 185, G: 0, B: 0, A: 255}
+	blue       = rl.Color{R: 0, G: 69, B: 173, A: 255}
+	orange     = rl.Color{R: 255, G: 89, B: 0, A: 255}
+	white      = rl.Color{R: 255, G: 255, B: 255, A: 255}
+	yellow     = rl.Color{R: 255, G: 213, B: 0, A: 255}
+	allColors  = map[int]rl.Color{
+		blackKey:  lightBlack,
 		greenKey:  green,
 		redKey:    red,
 		blueKey:   blue,
