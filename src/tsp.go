3ae62508155f8cd0db31b8022a191acae71172bc
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2026-05-30 04:21:43 +0200

    Finish camera axis rotations

--- src/tsp.go
+++ src/tsp.go
@@ -27,10 +27,24 @@ const (
 	gamePadId = int32(0)
 )
 
-func updateTspCamera(camera *rl.Camera) {
+func updateTspCameraX(camera *rl.Camera) {
 	rightX := rl.GetGamepadAxisMovement(gamePadId, rl.GamepadAxisRightX)
 	var rotation = rl.MatrixRotate(rl.GetCameraUp(camera), 1.5*rightX)
 	var view = rl.Vector3Subtract(InitialCameraPosition, camera.Target)
 	view = rl.Vector3Transform(view, rotation)
 	camera.Position = rl.Vector3Add(camera.Target, view)
+}
+
+func updateTspCameraY(camera *rl.Camera) {
+	rightY := rl.GetGamepadAxisMovement(gamePadId, rl.GamepadAxisRightY)
+	factor := If(rightY < 0, 1.5, 1)
+	var rotation = rl.MatrixRotate(rl.NewVector3(-1, 0.0, 1), float32(factor)*rightY)
+	var view = rl.Vector3Subtract(camera.Position, camera.Target)
+	view = rl.Vector3Transform(view, rotation)
+	camera.Position = rl.Vector3Add(camera.Target, view)
+}
+
+func updateTspCamera(camera *rl.Camera) {
+	updateTspCameraX(camera)
+	updateTspCameraY(camera)
 }
