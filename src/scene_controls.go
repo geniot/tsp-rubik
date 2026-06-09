78782021bf1bf6917b8b46aafda06298cf674577
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2026-06-09 08:28:30 +0200

    Render selection line conditionally based on gamepad availability

--- src/scene_controls.go
+++ src/scene_controls.go
@@ -35,8 +35,9 @@ func (cs *ControlsScene) Update(_ *rl.Camera) {
 	gui.SetStyle(gui.DEFAULT, gui.TEXT_ALIGNMENT_VERTICAL, int64(gui.TEXT_ALIGN_TOP))
 	gui.SetStyle(gui.DEFAULT, gui.TEXT_LINE_SPACING, 50)
 	padding := float32(125)
+	selectionLine := If(rl.IsGamepadAvailable(gamePadId), "[A,B,X,Y,L1,L2,R1,R2] Selection", "[1,2,3,4,5,6,7,8,9] Selection")
 	textBoxTextLines := []string{"GAME",
-		"[A,B,X,Y,L1,L2,R1,R2] Selection",
+		selectionLine,
 		"[Arrow keys, Left stick] Rotation",
 		"TUTORIAL",
 		"[MENU] Back to Menu",
@@ -44,8 +45,12 @@ func (cs *ControlsScene) Update(_ *rl.Camera) {
 			"[DOUBLE SELECT] Next tutorial"}
 	lineHeight := float32(70)
 	for i := 0; i < len(textBoxTextLines); i++ {
-		gui.TextBox(rl.Rectangle{padding, padding + lineHeight*float32(i), winWidth - padding*2,
-			lineHeight}, &textBoxTextLines[i], 64, false)
+		gui.TextBox(rl.Rectangle{
+			X:      padding,
+			Y:      padding + lineHeight*float32(i),
+			Width:  winWidth - padding*2,
+			Height: lineHeight},
+			&textBoxTextLines[i], 64, false)
 	}
 	gui.SetStyle(gui.DEFAULT, gui.TEXT_ALIGNMENT_VERTICAL, int64(gui.TEXT_ALIGN_CENTER))
 	gui.SetStyle(gui.DEFAULT, gui.TEXT_LINE_SPACING, 10)
